dfb4cf06274e9b301a5577c4bcfe72a19e32f200
ShayBudda <shayevanswow@gmail.com>
2024-07-28 20:09:24 -0700

    Add project files.

---
+++ LinkeList.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedLists
+{
+    internal class LinkedList
+    {
+        public Node First {  get; set; }
+
+        public void InsertFirst(int data)
+        {
+            //Create the Node
+            Node newNode = new Node();
+            //Put the data in the node
+            newNode.Data = data;
+            //put the old node in next
+            newNode.Next = First;
+            //Make the head the new node
+            First = newNode;
+
+        } // Most of the time deleting the old head is illogical. You just assign a new head.
+
+        public Node DeleteFirst()
+        {
+            //Assign the tempereary variable
+            Node temp = First;
+            //Assign the new head
+            First = First.Next;
+            return temp;
+        }
+        public void DisplayList()
+        {
+            Console.WriteLine("Iterating thru list.");
+            Node current = First;
+            while (current != null)
+            {
+                current.DisplayNode();
+                current = current.Next;
+            }
+        }
+        public void InsertLast(int data)
+        {
+            Node current = First;
+            while(current.Next != null)
+            {
+                current = current.Next;
+            }
+            Node newNode = new Node();
+            newNode.Data = data;
+            current.Next = newNode;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//Linked-List
+
+using LinkedLists;
+
+LinkedList LinkedList = new LinkedList();
+LinkedList.InsertFirst(1);
+LinkedList.InsertFirst(2);
+LinkedList.InsertFirst(3);
+LinkedList.InsertFirst(4);
+
+LinkedList.DeleteFirst();
+LinkedList.DeleteFirst();
+
+LinkedList.InsertLast(454);
+LinkedList.InsertLast(456);
+
+LinkedList.DisplayList();
+// a node is a part of a linked list that includes the element and the reference point
+// the first part of a linked list is null. It's like th kaboose of the train
+// This code is thanks to Teddy Smith's YouTube Channel
